6734e6d3b5c1d9da4829b662f6de86aa0f4089c7
Zzm0809 <934230207@qq.com>
2024-12-08 11:18:52 +0800

    处理 postgresql 的逻辑删除字段

--- dinky-admin/src/main/java/org/dinky/interceptor/PostgreSQLPrepareInterceptor.java
+++ dinky-admin/src/main/java/org/dinky/interceptor/PostgreSQLPrepareInterceptor.java
@@ -19,6 +19,8 @@
 
 package org.dinky.interceptor;
 
+import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.executor.parameter.ParameterHandler;
 import org.apache.ibatis.executor.statement.StatementHandler;
 import org.apache.ibatis.mapping.BoundSql;
 import org.apache.ibatis.plugin.Interceptor;
@@ -37,6 +39,7 @@ import java.util.Properties;
             method = "prepare",
             args = {Connection.class, Integer.class})
 })
+@Slf4j
 public class PostgreSQLPrepareInterceptor implements Interceptor {
     @Override
     public Object intercept(final Invocation invocation) throws Throwable {
@@ -45,6 +48,12 @@ public class PostgreSQLPrepareInterceptor implements Interceptor {
         Field field = boundSql.getClass().getDeclaredField("sql");
         field.setAccessible(true);
         field.set(boundSql, boundSql.getSql().replace("`", "\"").toLowerCase());
+        // 拿出逻辑删除的字段 is_delete 将 is_delete = 0 改为 is_delete = false 防止逻辑删除失效 用正则匹配 因为不能保证空格的位置
+        field.set(boundSql, boundSql.getSql().replaceAll("is_delete = 0", "is_delete = false"));
+        field.set(boundSql, boundSql.getSql().replaceAll("is_delete = 1", "is_delete = true"));
+        // 将 set is_delete = 0 改为 set is_delete = false 防止逻辑删除失效 用正则匹配 因为不能保证空格的位置
+        field.set(boundSql, boundSql.getSql().replaceAll("set is_delete = 0", "set is_delete = false"));
+        field.set(boundSql, boundSql.getSql().replaceAll("set is_delete = 1", "set is_delete = true"));
         return invocation.proceed();
     }
 
