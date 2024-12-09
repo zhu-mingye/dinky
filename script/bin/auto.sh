97c267d30730100a9b35f523dde135fc5542682a
Zzm0809 <934230207@qq.com>
2024-12-09 17:00:54 +0800

    [Bug] Fix automation script path issue (#4030)

--- script/bin/auto.sh
+++ script/bin/auto.sh
@@ -2,6 +2,7 @@
 # debug mode
 #set -x
 
+
 export RED='\033[31m'
 export GREEN='\033[32m'
 export YELLOW='\033[33m'
@@ -11,10 +12,20 @@ export CYAN='\033[36m'
 export RESET='\033[0m'
 
 
+ENV_FILE="/etc/profile.d/dinky_env"
+if [ -f "${ENV_FILE}" ]; then
+    source "${ENV_FILE}"
+fi
 
-if [ -z "${DINKY_HOME}" ]; then
-    echo -e "${RED}DINKY_HOME environment variable is not set. Attempting to determine the correct path...${RESET}"
+DB_ENV_FILE="/etc/profile.d/dinky_db"
+if [ -f "${DB_ENV_FILE}" ]; then
+    source "${DB_ENV_FILE}"
+fi
 
+source /etc/profile
+
+RETURN_HOME_PATH=""
+function get_home_path() {
     SOURCE="${BASH_SOURCE[0]}"
     while [ -h "$SOURCE" ]; do
         DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
@@ -22,16 +33,28 @@ if [ -z "${DINKY_HOME}" ]; then
         [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
     done
     DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
-    export DINKY_HOME="$(dirname "$DIR")"
+    RETURN_HOME_PATH=$(dirname "$DIR")
+}
+
+
+if [ -z "${DINKY_HOME}" ]; then
+    echo -e "${RED}DINKY_HOME environment variable is not set. Attempting to determine the correct path...${RESET}"
+    get_home_path
+    export DINKY_HOME=${RETURN_HOME_PATH}
     echo -e "${GREEN}DINKY_HOME is set to: ${DINKY_HOME}${RESET}"
 else
-    echo -e "${GREEN}DINKY_HOME is already set to: ${DINKY_HOME}${RESET}"
+    get_home_path
+    if [ "${DINKY_HOME}" != "${RETURN_HOME_PATH}" ]; then
+        export DINKY_HOME=${RETURN_HOME_PATH}
+        echo -e "${YELLOW}DINKY_HOME is not equal to the current path, reset DINKY_HOME to: ${RETURN_HOME_PATH}${RESET}"
+    else
+        echo -e "${GREEN}DINKY_HOME is already set to: ${DINKY_HOME}${RESET}"
+    fi
 fi
 
 
 FLINK_VERSION=${2}
 
-DINKY_HOME=${DINKY_HOME:-$(cd "$(dirname "$0")"; cd ..; pwd)}
 JAVA_VERSION=$(java -version 2>&1 | sed '1!d' | sed -e 's/"//g' | awk '{print $3}' | awk -F'.' '{print $1"."$2}')
 
 APP_HOME="${DINKY_HOME}"
@@ -70,6 +93,70 @@ assertIsInputVersion() {
     exit 1
   fi
 }
+
+
+if [ -f "${APP_HOME}/config/application.yml" ]; then
+  result=$("${APP_HOME}"/bin/parse_yml.sh "${APP_HOME}/config/application.yml" "server.port")
+  APP_PORT=$result
+fi
+
+echo -e "${GREEN}From ${APP_HOME}/config/application.yml server.port: ${APP_PORT}${RESET}"
+
+if [ -z "$APP_PORT" ]; then
+    echo -e "${RED}Could not find server.port in configuration files, using default port 8888 ${RESET}"
+    APP_PORT=8888
+fi
+
+# Function: Check the status of the health check endpoint
+check_health() {
+    curl --silent --max-time 2 --output /dev/null --write-out "%{http_code}" "http://localhost:$APP_PORT/actuator/health"
+}
+
+
+
+format_time() {
+    local seconds=$1
+    local hours=$((seconds / 3600))
+    local minutes=$(( (seconds % 3600) / 60 ))
+    local remaining_seconds=$((seconds % 60))
+    printf "%02d:%02d:%02d" $hours $minutes $remaining_seconds
+}
+
+function wait_start_process() {
+    echo -e "${GREEN}>>>>>>>>>>>>>>>>>>>>> Starting application... <<<<<<<<<<<<<<<<<<<<<<<${RESET}"
+     local max_attempts=100
+     local attempt=0
+     local delay=0.25
+     local health_status=""
+     local success_status_codes=("200")
+     local start_time=$(date +%s)
+
+     while [ $attempt -lt $max_attempts ]; do
+         attempt=$((attempt + 1))
+         local current_time=$(date +%s)
+         local elapsed_time=$((current_time - start_time))
+         local formatted_time=$(format_time $elapsed_time)
+         health_status=$(check_health)
+         for code in "${success_status_codes[@]}"; do
+             if [ "$health_status" == "$code" ]; then
+                 echo -ne "\r[==================================================] 100%\n"
+                 echo -e "${GREEN}Application started completed.${RESET}"
+                 return 0
+             fi
+         done
+         local progress=$((attempt * 100 / max_attempts))
+         local bar_length=50
+         local filled_length=$((progress * bar_length / 100))
+         local empty_length=$((bar_length - filled_length))
+         local bar=$(printf '>%.0s' $(seq 1 $filled_length))$(printf ' %.0s' $(seq 1 $empty_length))
+         echo -ne "\r[${bar}] ${progress}% (time consuming: ${formatted_time})"
+         sleep $delay
+     done
+     echo -ne "\r[==================================================] 100% (time consuming: ${formatted_time})\n"
+     echo -e "${RED}Application start failed. Please check the log for details.${RESET}"
+     return 1
+}
+
 
 # Use FLINK_HOME:
 CLASS_PATH="${APP_HOME}:${APP_HOME}/lib/*:${APP_HOME}/config:${EXTENDS_HOME}/*:${CUSTOMER_JAR_PATH}/*:${EXTENDS_HOME}/flink${FLINK_VERSION}/dinky/*:${EXTENDS_HOME}/flink${FLINK_VERSION}/flink/*:${EXTENDS_HOME}/flink${FLINK_VERSION}/*"
@@ -129,7 +216,9 @@ start() {
   updatePid
   if [ -z "$pid" ]; then
     nohup java ${PARAMS_OPT} ${JVM_OPTS} ${OOM_OPT} ${GC_OPT} -Xverify:none -cp "${CLASS_PATH}" org.dinky.Dinky ${JAR_PARAMS_OPT}  > ${DINKY_LOG_PATH}/dinky-start.log 2>&1 &
-    echo $! >"${PID_PATH}"/${PID_FILE}
+    PID=$!
+    echo "${PID}" >"${PID_PATH}"/${PID_FILE}
+    wait_start_process
     echo -e "${GREEN}........................................Start Dinky Successfully........................................${RESET}"
     echo -e "${GREEN}current log path : ${DINKY_LOG_PATH}/dinky-start.log , you can execute tail -fn1000 ${DINKY_LOG_PATH}/dinky-start.log to watch the log${RESET}"
   else
@@ -153,9 +242,11 @@ startWithJmx() {
   updatePid
   if [ -z "$pid" ]; then
     nohup java ${PARAMS_OPT} ${JVM_OPTS} ${OOM_OPT} ${GC_OPT} -Xverify:none "${JMX}" -cp "${CLASS_PATH}" org.dinky.Dinky  ${JAR_PARAMS_OPT}  > ${DINKY_LOG_PATH}/dinky-start.log 2>&1 &
-#    echo $! >"${PID_PATH}"/${PID_FILE}
-    updatePid
+    PID=$!
+    wait_start_process
     echo -e "$GREEN........................................Start Dinky with Jmx Successfully........................................$RESET"
+    updatePid
+
   else
     echo -e "$YELLOW Dinky pid $pid is in ${PID_PATH}/${PID_FILE}, Please stop first !!!$RESET"
   fi
